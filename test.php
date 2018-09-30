78b6d8e62829d37e39380f7b150707b69de04e1d
gaojing <3427911052@qq.com>
2018-09-30 12:00:05 +0800

    hello

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+require "./umb.php";
+$str = "this string is not modified";
+$loops = 1000000;
+micro_benchmark('str_replace', 'bm_str_replace', $loops);
+micro_benchmark('preg_replace', 'bm_preg_replace', $loops);
+
+function bm_str_replace($loops){
+    global $str;
+    for($i = 0;$i<$loops;$i++){
+        str_replace("is not", "has been",$str);
+    }
+}
+
+function bm_preg_replace($loops){
+    global $str;
+    for($i = 0;$i<$loops;$i++){
+        preg_replace("/is not/", "has been",$str);
+    }
+}
